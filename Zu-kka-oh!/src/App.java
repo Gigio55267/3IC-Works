b88c918c6958a7906595bcca497ad7d657cf3008
Riccardo <riccardosaverino2009@gmail.com>
2026-01-23 22:49:11 +0100

    commit

--- Zu-kka-oh!/src/App.java
+++ Zu-kka-oh!/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World sddf!");
+        Carta provCarta=new Carta();
+        System.out.println(provCarta.getNome());
+        System.out.println(provCarta.getAtk());
     }
 }
--- Zu-kka-oh!/src/Carta.java
+++ Zu-kka-oh!/src/Carta.java
@@ -1,5 +1,5 @@
 public class Carta {
-    private String nome;
+    private final String nome;
     private int puntiVita;
     private int atk;
     private int def;
@@ -11,8 +11,9 @@ public class Carta {
     "Antico", "Furioso", "Mistico", "Selvaggio", "Celeste","Infernale","Glaciale",
     "Tonante","Sacro", "Maledetto","Supremo", "Eterno", "Fatale", "Divino", "Spettrale"
     };
+    private int getDef;
     public Carta() {
-        this.nome = nome;
+        this.nome = creaNome(prefissi, suffissi);
         this.puntiVita = creaStatistiche();
         this.atk = creaStatistiche();
         this.def = creaStatistiche();
@@ -32,8 +33,32 @@ public class Carta {
         return def;
     }
     public static int creaStatistiche(){
-        int stat=(int)Math.random()*(50-0)+0;
+        int stat=(int)(Math.random()*(50-0)+0);
         return stat;
     }
-    
+    private static String creaNome(String[] pre,String[] suf){
+        int numpre=(int)(Math.random()*(15-1)+1);
+        int numsuf=(int)(Math.random()*(15-1)+1);
+        String spazio=" ";
+        String nome = pre[numpre]+spazio+suf[numsuf];
+        return nome;
+    }
+    private static int[] sistemazioneStatistiche(int[]Statistiche){
+        int[] stat = Statistiche;
+        int somma = stat[0]+stat[1]+ stat[2];
+        while (somma>100) {
+            stat[0]=stat[0]-1;
+            stat[1]=stat[1]-1;
+            stat[2]=stat[2]-1;
+            somma = stat[0]+stat[1]+ stat[2];
+        }
+        return stat;
+    }
+    private static boolean controlloStatistiche(int[]Statistiche){
+        int somma =Statistiche[0]+Statistiche[1]+Statistiche[2];
+        if (somma>100) {
+            return false;            
+        }
+        return true;
+    }
 }
