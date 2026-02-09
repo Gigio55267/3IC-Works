ca3781755ded565804ae4e244921a18066ff5571
Riccardo Saverino <riccardosaverino09@gmail.com>
2026-02-09 12:46:37 +0100

    commit

--- Zu-kka-oh!/src/Giocatore.java
+++ Zu-kka-oh!/src/Giocatore.java
@@ -6,6 +6,7 @@ public class Giocatore {
     protected Carta[] campo;
     protected int vita;
     private final Target target;
+
     public Giocatore(String nomegiocatore) {
         this.nomegiocatore = nomegiocatore;
         this.mazzo = creaMazzo();
@@ -14,128 +15,140 @@ public class Giocatore {
         this.vita = 3;
         this.target = Target.targetRandom();
     }
+
     public String getNomegiocatore() {
         return nomegiocatore;
     }
+
     public Carta[] getMazzo() {
         return mazzo;
     }
+
     public Carta[] getMano() {
         return mano;
     }
+
     public Carta[] getCarteincampo() {
         return campo;
     }
+
     public int getVita() {
         return vita;
     }
+
     public Target getTarget() {
         return target;
     }
-    private static Carta[] creaMazzo(){
-        Carta[] mazzo=new Carta[50];
+
+    private static Carta[] creaMazzo() {
+        Carta[] mazzo = new Carta[50];
         for (int i = 0; i < mazzo.length; i++) {
-            mazzo[i]=new Carta();
+            mazzo[i] = new Carta();
         }
         return mazzo;
     }
-    private static Carta[] prendiPrimaMano(Carta[]mazzo){
-        Carta[] mano=new Carta[10];
-        int contatore=49;
+
+    private static Carta[] prendiPrimaMano(Carta[] mazzo) {
+        Carta[] mano = new Carta[10];
+        int contatore = 49;
         for (int i = 0; i < 5; i++) {
-            mano[i]=mazzo[contatore];
-            mazzo[contatore]=null;
+            mano[i] = mazzo[contatore];
+            mazzo[contatore] = null;
             contatore--;
         }
         return mano;
     }
-    public int spazioCampo(){
+
+    public int spazioCampo() {
         int counter = 0;
         for (int i = 0; i < campo.length; i++) {
-            if (campo[i] == null){
-                counter ++;
+            if (campo[i] == null) {
+                counter++;
             }
         }
         return counter;
     }
-    public void  pescaCarte(Carta[]mazzo,Carta[]mano){
+
+    public void pescaCarte(Carta[] mazzo, Carta[] mano, int turno) {
+        int turnoatt=turno-1;
         for (int i = 0; i < mano.length; i++) {
-            if (mano[i]==null) {
-                for (int j = 0; j < mazzo.length; j++) {
-                    if (mazzo[j]!=null) {
-                        mano[i]=mazzo[j];
-                        System.out.println("hai pescato");
+            if (mano[i] == null) {
+                // for (int j = 0; j < mazzo.length; j++) {
+                //if (mazzo[turno-5] != null) {
+                    mano[i] = mazzo[turnoatt-1];
+                    System.out.println("hai pescato");
+                }
+                break;
+            //}
+            //break;
+        }
+        // }
+        mano = Utility.insertionSortVita(mano);
+    }
+
+    public void evocaCarta(Carta[] mano, Carta[] campo) {
+        int indexCartaDaPiazzare = 0;
+        Carta[] carte = mano;
+        for (int i = 0; i < carte.length; i++) {
+            if (carte[i] != null) {
+                indexCartaDaPiazzare = i;
+                System.out.println("hai evocato " + carte[i].getNome());
+                for (int j = 0; j < campo.length; j++) {
+                    if (campo[j] == null) {
+                        campo[j] = carte[indexCartaDaPiazzare];
                         break;
                     }
-                    break;
                 }
+                carte[i] = null;
                 break;
             }
         }
-        mano=Utility.insertionSortVita(mano);
     }
-    public void evocaCarta(Carta[] mano,Carta[] campo){
-        int indexCartaDaPiazzare=0;
-        Carta[] carte = mano;
-        for (int i = 0; i < carte.length; i++) {
-            if (carte[i]!=null) {
-                indexCartaDaPiazzare=i;
-               System.out.println("hai evocato "+carte[i].getNome());
-               for (int j = 0; j < campo.length; j++) {
-                if (campo[j]==null) {
-                campo[j]=carte[indexCartaDaPiazzare];
-                break;
-                }
-            }
-            carte[i]=null;   
-            break;
-            } 
-        }  
-    }
-    public void attacca( Giocatore avversario){
+
+    public void attacca(Giocatore avversario) {
         for (int i = 0; i < campo.length; i++) {
-            if (campo[i] != null){
-                if (avversario.spazioCampo() == 5){
-                    if (avversario.vita > 0){
+            if (campo[i] != null) {
+                if (avversario.spazioCampo() == 5) {
+                    if (avversario.vita > 0) {
                         avversario.vita--;
-                        System.out.println(avversario.nomegiocatore + " non ha carte in campo. " + this.nomegiocatore + " infligge 1 danno diretto.");
-                    }
-                    else {
+                        System.out.println(avversario.nomegiocatore + " non ha carte in campo. " + this.nomegiocatore
+                                + " infligge 1 danno diretto.");
+                    } else {
                         System.out.println(avversario.nomegiocatore + " è morto.");
                         return;
                     }
-                }
-                else {
+                } else {
                     Carta cartaSottoAttacco = avversario.campo[campo[i].trovaTarget(campo[i].target, avversario.campo)];
-                    
-                        if (campo[i].atk - cartaSottoAttacco.def > 1) {
-                            int dannoInflitto = campo[i].atk - cartaSottoAttacco.def;
-                            cartaSottoAttacco.puntiVita -= dannoInflitto;
-                            System.out.println(campo[i].nome + " attacca " + cartaSottoAttacco.nome + " infliggendo " + dannoInflitto + " danni.");
-                        }
-                        else {
-                            cartaSottoAttacco.puntiVita -= 1;
-                            System.out.println(campo[i].nome + " attacca " + cartaSottoAttacco.nome + " infliggendo 1 danno.");
-                        }
-                        if (cartaSottoAttacco.puntiVita <= 0){
+
+                    if (campo[i].atk - cartaSottoAttacco.def > 1) {
+                        int dannoInflitto = campo[i].atk - cartaSottoAttacco.def;
+                        cartaSottoAttacco.puntiVita -= dannoInflitto;
+                        System.out.println(campo[i].nome + " attacca " + cartaSottoAttacco.nome + " infliggendo "
+                                + dannoInflitto + " danni.");
+                    } else {
+                        cartaSottoAttacco.puntiVita -= 1;
+                        System.out.println(
+                                campo[i].nome + " attacca " + cartaSottoAttacco.nome + " infliggendo 1 danno.");
+                    }
+                    if (cartaSottoAttacco.puntiVita <= 0) {
                         System.out.println(cartaSottoAttacco.nome + " è stato distrutto!");
                         for (int j = 0; j < avversario.campo.length; j++) {
-                            if (avversario.campo[j] == cartaSottoAttacco){
+                            if (avversario.campo[j] == cartaSottoAttacco) {
                                 avversario.campo[j] = null;
                                 break;
-                            }
-                            else {
-                            System.out.println(cartaSottoAttacco.nome + " ha " + cartaSottoAttacco.puntiVita + " HP rimanenti.");
+                            } else {
+                                System.out.println(cartaSottoAttacco.nome + " ha " + cartaSottoAttacco.puntiVita
+                                        + " HP rimanenti.");
                             }
                         }
-                        
+
                     }
-                    }
-                    }
-                    
-                    
+                }
             }
+
         }
+    }
+
     public void setVita(int vita) {
         this.vita = vita;
     }
--- Zu-kka-oh!/src/Partita.java
+++ Zu-kka-oh!/src/Partita.java
@@ -1,71 +1,79 @@
 
 public class Partita {
-private Giocatore giocatore1;
-private Giocatore giocatore2;
-private int turno;
-private boolean primoTurno;
-public Partita(String nome1, String nome2) {
-giocatore1 = new Giocatore(nome1);
-giocatore2 = new Giocatore(nome2);
-turno = 0;
-primoTurno = true;
-}
-// Gioca 1 solo turno. Se `turno` è pari, tocca al giocatore1,
-// altrimenti tocca al giocatore2
-public void turno() {
-    
-    //System.out.println();
-    if (primoTurno){
-        primoTurno=false;
-        //System.out.println(primoTurno);
-        System.out.println("turno di "+giocatore1.getNomegiocatore());
-        giocatore1.pescaCarte(giocatore1.getMazzo(), giocatore1.getMano());
-        giocatore1.evocaCarta(giocatore1.getMano(), giocatore1.getCarteincampo());}
-        else if (turno % 2 == 0){
-            System.out.println("turno di "+giocatore1.getNomegiocatore());
-        giocatore1.pescaCarte(giocatore1.getMazzo(), giocatore1.getMano());
-        //System.out.println(Arrays.toString(giocatoreCorrente.getMano()) );
-        giocatore1.evocaCarta(giocatore1.getMano(), giocatore1.getCarteincampo()); 
-        //System.out.println("giocatore corrente "+Arrays.toString(giocatore1.getCarteincampo()));
-        giocatore1.attacca( giocatore2);
-        //System.out.println("avversario "+Arrays.toString(giocatore2.getCarteincampo())); 
-        //System.out.println(giocatore1.getVita());
-        //System.out.println("mano "+ Arrays.toString(giocatore1.getMano()));
-        }else {
-        System.out.println("turno di "+giocatore2.getNomegiocatore());
-        giocatore2.pescaCarte(giocatore2.getMazzo(), giocatore2.getMano());
-        //System.out.println(Arrays.toString(giocatoreCorrente.getMano()) );
-        giocatore2.evocaCarta(giocatore2.getMano(), giocatore2.getCarteincampo()); 
-        //System.out.println("giocatore corrente "+Arrays.toString(giocatore2.getCarteincampo()));
-        giocatore2.attacca( giocatore1);
-       // System.out.println("avversario "+Arrays.toString(giocatore1.getCarteincampo())); 
-       // System.out.println(giocatore2.getVita());
-       // System.out.println("mano "+ Arrays.toString(giocatore2.getMano()));
-    }     
- }
-    
+    private Giocatore giocatore1;
+    private Giocatore giocatore2;
+    private int turno;
+    private boolean primoTurno;
 
-// Controlla se la partita è finita
-public boolean isFinita() {
-    return giocatore1.vita <= 0 || giocatore2.vita <= 0;
- }
-// Ritorna il vincitore
-public Giocatore getVincitore() {
-    if (giocatore1.getVita() <=0){
-        return giocatore2;
-    } else if (giocatore2.getVita() <=0){
-        return giocatore1;
-    }else {
-        return null;
+    public Partita(String nome1, String nome2) {
+        giocatore1 = new Giocatore(nome1);
+        giocatore2 = new Giocatore(nome2);
+        turno = 0;
+        primoTurno = true;
     }
- }
-// Simula l'intera partita (vedi dopo)
-public void gioca() {
-    System.out.println("inizio della partita tra "+giocatore1.getNomegiocatore()+" e "+giocatore2.getNomegiocatore());
-    while (!isFinita()) {
-        turno();
-        turno++;
+
+    // Gioca 1 solo turno. Se `turno` è pari, tocca al giocatore1,
+    // altrimenti tocca al giocatore2
+    public void turno() {
+
+        // System.out.println();
+        if (primoTurno) {
+            primoTurno = false;
+            // System.out.println(primoTurno);
+            System.out.println("turno di " + giocatore1.getNomegiocatore());
+            giocatore1.pescaCarte(giocatore1.getMazzo(), giocatore1.getMano(), this.turno);
+            giocatore1.evocaCarta(giocatore1.getMano(), giocatore1.getCarteincampo());
+        } else if (turno % 2 == 0) {
+            System.out.println("turno di " + giocatore1.getNomegiocatore());
+            giocatore1.pescaCarte(giocatore1.getMazzo(), giocatore1.getMano(), this.turno);
+            // System.out.println(Arrays.toString(giocatoreCorrente.getMano()) );
+            giocatore1.evocaCarta(giocatore1.getMano(), giocatore1.getCarteincampo());
+            // System.out.println("giocatore corrente
+            // "+Arrays.toString(giocatore1.getCarteincampo()));
+            giocatore1.attacca(giocatore2);
+            // System.out.println("avversario
+            // "+Arrays.toString(giocatore2.getCarteincampo()));
+            // System.out.println(giocatore1.getVita());
+            // System.out.println("mano "+ Arrays.toString(giocatore1.getMano()));
+        } else {
+            System.out.println("turno di " + giocatore2.getNomegiocatore());
+            giocatore2.pescaCarte(giocatore2.getMazzo(), giocatore2.getMano(),this.turno);
+            // System.out.println(Arrays.toString(giocatoreCorrente.getMano()) );
+            giocatore2.evocaCarta(giocatore2.getMano(), giocatore2.getCarteincampo());
+            // System.out.println("giocatore corrente
+            // "+Arrays.toString(giocatore2.getCarteincampo()));
+            giocatore2.attacca(giocatore1);
+            // System.out.println("avversario
+            // "+Arrays.toString(giocatore1.getCarteincampo()));
+            // System.out.println(giocatore2.getVita());
+            // System.out.println("mano "+ Arrays.toString(giocatore2.getMano()));
+        }
     }
-    System.out.println("il vincitore è:"+(getVincitore().nomegiocatore));
- }
+
+    // Controlla se la partita è finita
+    public boolean isFinita() {
+        return giocatore1.vita <= 0 || giocatore2.vita <= 0;
+    }
+
+    // Ritorna il vincitore
+    public Giocatore getVincitore() {
+        if (giocatore1.getVita() <= 0) {
+            return giocatore2;
+        } else if (giocatore2.getVita() <= 0) {
+            return giocatore1;
+        } else {
+            return null;
+        }
+    }
+
+    // Simula l'intera partita (vedi dopo)
+    public void gioca() {
+        System.out.println(
+                "inizio della partita tra " + giocatore1.getNomegiocatore() + " e " + giocatore2.getNomegiocatore());
+        while (!isFinita()) {
+            turno();
+            turno++;
+        }
+        System.out.println("il vincitore è:" + (getVincitore().nomegiocatore));
+    }
 }
