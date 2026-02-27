800c48844f7bba99c120e97fa64e265c50a6d157
Riccardo Saverino <riccardosaverino09@gmail.com>
2026-02-27 13:49:43 +0100

    commit # esercizio sui fiocchi

--- disegni/src/FigureFrattali.java
+++ disegni/src/FigureFrattali.java
@@ -16,13 +16,37 @@ public class FigureFrattali extends TurtleScreen{
             
         }
     }
+    public void curvaCoch(int livello, double lunghezza){
+        if (livello==0)t.forward(lunghezza);
+        else {
+            curvaCoch(livello-1, lunghezza/3);
+            t.left(60);
+            curvaCoch(livello-1, lunghezza/3);
+            t.right(120);
+            curvaCoch(livello-1, lunghezza/3);
+            t.left(60);
+            curvaCoch(livello-1, lunghezza/3);
+        }
+    }
     public void fioccoDiNeveDiKoch(){
-
+        for (int i = 0; i < 3; i++) {
+            curvaCoch(5, lunghezza);
+            t.right(120);
+        }
     }
     @Override
     public void setup() {
         noLoop();
         t = createTurtle();
+        t.penDown();
+        t.speed(0);
+        fioccoDiNeveDiKoch();
+        
+    }
+     public static void main(String[] args) {
+
+        FigureFrattali poligonoDisegnato = new FigureFrattali(200);
+        poligonoDisegnato.run();
     }
 
 }
